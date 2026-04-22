ec77d9d0c541522517af6b9e0f58c6cf63ede9a6
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2026-04-22 17:25:12 +0800

    Fix excessive check

--- contents/mods/AnotherVehicleClaimSystemB41/media/lua/server/AVCSServer.lua
+++ contents/mods/AnotherVehicleClaimSystemB41/media/lua/server/AVCSServer.lua
@@ -337,7 +337,7 @@ function AVCS.doClaimTimeout()
 	local needSort = false
 	-- As we dealing with indexes, we want to control the index value as we increment to avoid removing wrong index
 	while varIndex <= #AVCS.sortedPlayerTimeoutClaim do
-		if getTimestamp() > AVCS.sortedPlayerTimeoutClaim[varIndex].ExpiryTime then
+		if getTimestamp() > AVCS.sortedPlayerTimeoutClaim[varIndex].ExpiryTime + (SandboxVars.AVCS.ClaimTimeout * 60 * 60) then
 			if AVCS.dbByPlayerID[AVCS.sortedPlayerTimeoutClaim[varIndex].OwnerPlayerID] ~= nil then
 				-- Cache is not always up-to-date, validate the actual
 				if getTimestamp() > (AVCS.dbByPlayerID[AVCS.sortedPlayerTimeoutClaim[varIndex].OwnerPlayerID].LastKnownLogonTime + (SandboxVars.AVCS.ClaimTimeout * 60 * 60)) then
