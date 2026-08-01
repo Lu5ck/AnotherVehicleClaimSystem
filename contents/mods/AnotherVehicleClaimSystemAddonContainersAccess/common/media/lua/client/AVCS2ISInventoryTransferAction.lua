78b20fcebc47c99cd631d401eafd8321d464b31b
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2026-08-02 02:50:57 +0800

    B42: Fix nested first level container

--- contents/mods/AnotherVehicleClaimSystemAddonContainersAccess/common/media/lua/client/AVCS2ISInventoryTransferAction.lua
+++ contents/mods/AnotherVehicleClaimSystemAddonContainersAccess/common/media/lua/client/AVCS2ISInventoryTransferAction.lua
@@ -4,6 +4,16 @@ function ISInventoryTransferAction:isValid()
     if not self.srcContainer then return AVCS.oISInventoryTransferActionValid(self) end
 
     local vehiclePart = self.srcContainer:getVehiclePart()
+    if not vehiclePart then
+        local item = self.srcContainer:getContainingItem()
+        if item then
+            local itemParentContainer = item:getContainer()
+            if itemParentContainer then
+                vehiclePart = itemParentContainer:getVehiclePart()
+            end
+        end
+    end
+
     if vehiclePart then
         local checkResult = AVCS.getPublicPermission(vehiclePart:getVehicle(), "AllowContainersAccess")
         -- If public allowed, we don't have to check other permissions
